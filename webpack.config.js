76a4ad210d0a62265a2d44067b70690da95b91b5
Benji Nguyen <benjamin.van.nguyen@gmail.com>
2022-07-31 01:31:22 -0700

    webpack + babel + postcss

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const { resolve } = require("path");
+
+module.exports = (env, args) => {
+  const CWD = process.cwd();
+  const production = (args.mode === "production");
+
+  return {
+    entry: resolve(CWD, "assets/javascript/entry/application_v1.tsx"), 
+
+    output: {
+        path: resolve(CWD, "public"),
+        filename: production ? "[name].[contenthash].js" : "[name].[hash].js",
+    },
+
+    devServer: {
+      static: {
+        directory: resolve(CWD, "public"),
+        publicPath: "/static",
+      },
+      hot: true
+    },
+
+    module: {
+      rules: [
+        // CSS
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: [
+            { loader: 'style-loader', },
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            { loader: 'postcss-loader' }
+          ]
+        },
+        // TypeScript
+        {
+          test: /\.(js|jsx|tsx|ts)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript"
+              ]
+            }
+          }
+        },
+      ]
+    },
+
+    resolve: {
+      extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: resolve(CWD, "tsconfig.json"),
+          extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+        })
+      ]
+    },
+
+    plugins: [
+      new ForkTsCheckerWebpackPlugin({ async: false }),
+    ],
+  }
+}
